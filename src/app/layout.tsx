3580daac7e8e9eb80bf212897284fb7bb5d00b3c
Shannon <shannon.stefanus@gmail.com>
2024-05-08 13:02:44 -0700

    first commit - FAQ + landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,30 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+import localfont from 'next/font/local'
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const pptelegraflight = localfont({
+  src: [
+    {
+      path: "../../public/font/PPTelegraf-UltraLight.otf", 
+      weight: "300", 
+    }
+  ], 
+  variable: "--font-pptelegraflight"
+})
+
+const pptelegraf = localfont({
+  src: [
+    {
+      path: "../../public/font/PPTelegraf-UltraBold.otf", 
+      weight: "700", 
+    },
+  ], 
+  variable: "--font-pptelegraf", 
+})
+
+const inter = Poppins({weight:'700', 
+                      subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +37,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={'${pptelegraf.variable} font-sansmono'}>
+      <body>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+
+      fontFamily: {
+        pptelegraf: ["var(--font-pptelegraf)"], 
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
