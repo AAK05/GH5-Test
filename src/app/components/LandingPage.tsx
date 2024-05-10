074b2adf4ec10c2ac2c31cf751b3c50ffa0508fe
AAK05 <adrien.kusuma@gmail.com>
2024-05-10 23:22:36 +0700

    changed background colour to be a different shade of black; fixed background gradient images

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const LandingPage = () => {
   return (
-        <main className="min-h-screen bg-black relative overflow-y-auto">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col md:flex-row items-center mb-8">
-          <img src="logo.png" alt="Logo" className="h-20 mb-4" />
+        <main className="min-h-screen bg-black relative overflow-y-clip">
+      <div className="absolute top-[40%] lg:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col lg:flex-row items-center mb-8">
+          <img src="logo.png" alt="Logo" className="h-40 lg:h-24 mb-4 lg:mb-0" />
           <div className="flex flex-col text-left">
             <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-red-300 via-purple-500 to-gray-100 mb-0 text-6xl font-extrabold font-pptelegraf">
               Garuda Hacks 5.0
@@ -16,7 +16,7 @@ const LandingPage = () => {
             </h2>
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center z-10">
           <button className="text-white font-pptelegraflight px-8 py-4 border border-white bg-black-500 rounded-lg shadow-md hover:bg-blue-600 transition duration-300 ease-in-out">
             Coming Soon 
           </button>
@@ -25,15 +25,24 @@ const LandingPage = () => {
 
 
       {/* Background image */}
-      {/* <div className="absolute bottom-0 left-0 right-0">
+       <div className="absolute bottom-0 left-0 w-[80%] -translate-x-[40%] lg:w-full lg:-translate-x-[20%] lg:translate-y-[30%] z-0">
         <Image 
           src="/gradient.png"
-          width={798}
-          height={864}
-          alt="Picture of the author"
-          className="fixed bottom-0 left-0 right-0"
+          width={1197}
+          height={1296}
+          alt="Gradients"
+          className="z-0"
         />
-      </div> */}
+      </div>
+      <div className="absolute top-0 right-0 w-[80%] translate-x-[40%] lg:w-full lg:translate-x-[50%] lg:-translate-y-[30%] z-0">
+        <Image 
+          src="/gradient2.png"
+          width={1197}
+          height={1296}
+          alt="Gradients"
+          className="z-0"
+        />
+      </div>
     </main>
   )
 }
