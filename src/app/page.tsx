d02de04d31921e4fff496eedb46b0107ba90c2db
AAK05 <adrien.kusuma@gmail.com>
2024-05-12 23:54:41 +0700

    Created tracks page and added to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import FAQs from "./components/FAQs";
 import LandingPage from "./components/LandingPage"
+import Tracks from "./components/Tracks"
 
 export default function Home() {
   return (
     <section className="bg-black">
     <LandingPage />
+    <Tracks />
     <FAQs /> 
     </section>
   );
